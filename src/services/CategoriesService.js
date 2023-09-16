a8901ec1ea9a7e79cf0cef429f1efcd727a418c3
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2023-09-15 22:34:23 -0300

    :sparkles: Use category mapper

--- src/services/CategoriesService.js
+++ src/services/CategoriesService.js
@@ -1,12 +1,14 @@
 import HttpClient from './helpers/HttpClient';
+import CategoryMapper from './mappers/CategoryMapper';
 
 class CategoriesService {
   constructor() {
     this.httpClient = new HttpClient(import.meta.env.VITE_API);
   }
 
-  listCategories() {
-    return this.httpClient.get('/categories');
+  async listCategories() {
+    const categories = await this.httpClient.get('/categories');
+    return categories.map(CategoryMapper.toDomain);
   }
 }
 
