a54d9d317907fddc9a510855236cd196a219cc40
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2023-09-21 02:01:16 -0300

    :recycle: Removes the destructuring of handleAnimationEnd, the end of the animation is handled within the useAnimetedList hook

--- src/components/Toast/ToastContainer/index.jsx
+++ src/components/Toast/ToastContainer/index.jsx
@@ -8,7 +8,6 @@ export default function ToastContainer() {
   const {
     setItems: setMessages,
     handleRemoveItem,
-    handleAnimationEnd,
     renderList,
   } = useAnimetedList();
 
@@ -27,13 +26,13 @@ export default function ToastContainer() {
 
   return (
     <Container>
-      {renderList((message, { isLeaving }) => (
+      {renderList((message, { isLeaving, animatedRef }) => (
         <ToastMessage
           key={message.id}
           message={message}
           onRemoveMessage={handleRemoveItem}
           isLeaving={isLeaving}
-          onAnimationEnd={handleAnimationEnd}
+          animatedRef={animatedRef}
         />
       ))}
     </Container>
