88ca880387329c829730f90c93fd7d8fc55e81b4
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2023-09-21 02:04:55 -0300

    :recycle: Removes the event listener creation logic for the useAnimetedList hook

--- src/components/Toast/ToastMessage/index.jsx
+++ src/components/Toast/ToastMessage/index.jsx
@@ -1,59 +1,41 @@
 import PropTypes from 'prop-types';
-import { useEffect, useRef } from 'react';
+import { useEffect, memo } from 'react';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import { Container } from './styles';
 
-export default function ToastMessage({
-  message,
-  onRemoveMessage,
-  isLeaving,
-  onAnimationEnd,
-}) {
-  const animatedElementRef = useRef(null);
+const ToastMessage = memo(
+  ({ message, onRemoveMessage, isLeaving, animatedRef }) => {
+    useEffect(() => {
+      const timeoutId = setTimeout(() => {
+        onRemoveMessage(message.id);
+      }, message.duration || 7000);
 
-  useEffect(() => {
-    const elementRef = animatedElementRef.current;
+      return () => clearTimeout(timeoutId);
+    }, [message, onRemoveMessage]);
 
-    function handleAnimationEnd() {
-      onAnimationEnd(message.id);
-    }
-
-    if (isLeaving) {
-      elementRef.addEventListener('animationend', handleAnimationEnd);
-    }
-
-    return () =>
-      elementRef.removeEventListener('animationend', handleAnimationEnd);
-  }, [isLeaving, message.id, onAnimationEnd]);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
+    function handleRemoveToast() {
       onRemoveMessage(message.id);
-    }, message.duration || 7000);
+    }
 
-    return () => clearTimeout(timeoutId);
-  }, [message, onRemoveMessage]);
-
-  function handleRemoveToast() {
-    onRemoveMessage(message.id);
-  }
-
-  return (
-    <Container
-      tabIndex={0}
-      type={message.type}
-      onClick={handleRemoveToast}
-      role="button"
-      isLeaving={isLeaving}
-      ref={animatedElementRef}
-    >
-      {message.type === 'danger' && <img src={xCircleIcon} alt="X" />}
-      {message.type === 'success' && <img src={checkCircleIcon} alt="Check" />}
-      <strong>{message.text}</strong>
-    </Container>
-  );
-}
+    return (
+      <Container
+        tabIndex={0}
+        type={message.type}
+        onClick={handleRemoveToast}
+        role="button"
+        isLeaving={isLeaving}
+        ref={animatedRef}
+      >
+        {message.type === 'danger' && <img src={xCircleIcon} alt="X" />}
+        {message.type === 'success' && (
+          <img src={checkCircleIcon} alt="Check" />
+        )}
+        <strong>{message.text}</strong>
+      </Container>
+    );
+  },
+);
 
 ToastMessage.propTypes = {
   message: PropTypes.shape({
@@ -63,6 +45,10 @@ ToastMessage.propTypes = {
     duration: PropTypes.number,
   }).isRequired,
   onRemoveMessage: PropTypes.func.isRequired,
-  onAnimationEnd: PropTypes.func.isRequired,
   isLeaving: PropTypes.bool.isRequired,
+  animatedRef: PropTypes.shape().isRequired,
 };
+
+ToastMessage.displayName = 'ToastMessage';
+
+export default ToastMessage;
